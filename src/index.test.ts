0ce8886c1222c62bb6179ac20e5263c4218b793f
yoonjong.park <>
2021-11-26 11:42:27 +0900

    #8 CHANGE module type (function -> class) for Cleaningg

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,48 +1,49 @@
-import { RegistrationNumber } from '.'
-import '@testing-library/jest-dom/extend-expect'
+import RegistrationValidator from './index'
+
+const IdValidator = new RegistrationValidator()
 
 describe('주민번호체크', () => {
   it('주민등록번호 테스트', () => {
-    const result = RegistrationNumber('6407131018433', 'NATIVE')
+    const result = IdValidator.validateNative('6407131018433')
     expect(result).toBe(true)
   })
   it('주민등록번호 테스트 - 잘못된 번호를 넣었을 떄', () => {
-    const result = RegistrationNumber('9901015020063', 'NATIVE')
+    const result = IdValidator.validateNative('9901015020063')
     expect(result).toBe(false)
   })
   it('주민등록번호 테스트 - 더 긴 번호를 넣었을 때', () => {
-    const result = RegistrationNumber('99010150200631111', 'NATIVE')
+    const result = IdValidator.validateNative('99010150200631111')
     expect(result).toBe(false)
   })
   it('주민등록번호 테스트 - 더 짧은 번호를 넣었을 때', () => {
-    const result = RegistrationNumber('990101502', 'NATIVE')
+    const result = IdValidator.validateNative('990101502')
     expect(result).toBe(false)
   })
-  it('주민등록번호 테스트 - 맞는 주민번호, FOREIGNER 입력 시', () => {
-    const result = RegistrationNumber('6407131018433', 'FOREIGNER')
+  it('주민등록번호 테스트 - 맞는 외국인등록번호 입력 시', () => {
+    const result = IdValidator.validateNative('9901015020063')
     expect(result).toBe(false)
   })
 })
 
 describe('외국인등록번호 체크', () => {
   it('외국인등록번호 테스트', () => {
-    const result = RegistrationNumber('9901015020063', 'FOREIGNER')
+    const result = IdValidator.validateForeigner('9901015020063')
     expect(result).toBe(true)
   })
   it('외국인등록번호 테스트 - 잘못된 번호를 넣었을 때', () => {
-    const result = RegistrationNumber('6407131018433', 'FOREIGNER')
+    const result = IdValidator.validateForeigner('6407131018433')
     expect(result).toBe(false)
   })
   it('외국인등록번호 테스트 - 더 긴 번호를 넣었을 때', () => {
-    const result = RegistrationNumber('6407131018433111111', 'FOREIGNER')
+    const result = IdValidator.validateForeigner('6407131018433111111')
     expect(result).toBe(false)
   })
   it('외국인등록번호 테스트 - 더 짧은 번호를 넣었을 때', () => {
-    const result = RegistrationNumber('6407131', 'FOREIGNER')
+    const result = IdValidator.validateForeigner('6407131')
     expect(result).toBe(false)
   })
-  it('외국인등록번호 테스트 - 맞는 외국인등록번호, NATIVE 입력 시', () => {
-    const result = RegistrationNumber('9901015020063', 'NATIVE')
+  it('외국인등록번호 테스트 - 맞는 주민등록번호 입력 시', () => {
+    const result = IdValidator.validateForeigner('6407131018433')
     expect(result).toBe(false)
   })
 })
